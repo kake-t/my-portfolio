34d3e75e34d3629212dc566c857c5471a4bf23c9
kake-t <36560265+kake-t@users.noreply.github.com>
2025-01-23 13:09:35 +0000

    自画像を追加

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,9 +1,11 @@
 import { ProfileData } from "@/types";
+import nextConfig from "../../next.config.mjs";
+const BASE_PATH = nextConfig.basePath || "";
 
 export const profileData: ProfileData = {
-    name: "Your Name",
-    title: "Frontend Developer",
-    profileImage: "/api/placeholder/150/150",
+    name: "kake",
+    title: "Backend Developer",
+    profileImage: `${BASE_PATH}/icons/self_portrait_oil_painting.jpg`,
     socialLinks: [
         { platform: "github", url: "https://github.com/kake-t" },
         { platform: "zenn", url: "https://zenn.dev/kake99" },
